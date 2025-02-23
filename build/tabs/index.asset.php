466e108d76e68f85fd3926be3b0d8f7ab307c888
Rory <rory@fancysquares.com>
2025-02-23 13:17:09 -0800

    Update build files

--- build/tabs/index.asset.php
+++ build/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '54e8d70dc2c3683eccd5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '6ddcf2a15c96bb4bf9f8');
