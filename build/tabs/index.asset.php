8eda390ca80be14c8a37d0a6e10a7eb50b28cb30
Rory <rory@fancysquares.com>
2025-02-23 12:47:55 -0800

    Update main prod bulid

--- build/tabs/index.asset.php
+++ build/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '599902189bebaad75dcc');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '54e8d70dc2c3683eccd5');
