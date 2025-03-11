8a7f4ef1892a131d67355e47fdae2d0477093861
Rory <rory@fancysquares.com>
2025-03-10 19:10:03 -0700

    Picture block update
    Use FormToken fields for classes instead of selector

--- build/picture-block/index.asset.php
+++ build/picture-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'adacc0824d4311be317a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '63423204f68a96082298');
