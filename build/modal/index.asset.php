5b099d95f1a3e7b89f804cabe8725d46a3732007
Rory <rory@fancysquares.com>
2025-03-29 16:44:03 -0700

    Add settings for modal placement

--- build/modal/index.asset.php
+++ build/modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '09cd69d564b61013a9c8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0fc00076190f7c0c9fd5');
