4dfbb1bf071c93cc94435ff0745703c70ebe542c
Rory <rory@fancysquares.com>
2025-04-24 11:40:47 -0700

    updated production files

--- build/content-wrapper/index.asset.php
+++ build/content-wrapper/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6b676df87ff54e4883ab');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cdabef0edd111dbe9296');
