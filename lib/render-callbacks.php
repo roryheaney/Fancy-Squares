16009747698fe5c5abd600349cc00c19d4fda3b4
Rory <rory@fancysquares.com>
2025-04-24 10:43:19 -0700

    index and content-wrapper classes

--- lib/render-callbacks.php
+++ lib/render-callbacks.php
@@ -12,29 +12,6 @@ if (! defined('ABSPATH')) {
 }
 
 /**
- * Render callback for the container block.
- *
- * @param array  $attributes Block attributes.
- * @param string $content    Inner block markup.
- * @return string            HTML markup.
- */
-function fsblocks_render_container_block($attributes, $content)
-{
-	$additional_class = isset($attributes['additionalClass'])
-		? sanitize_html_class($attributes['additionalClass'])
-		: '';
-
-	$classes = 'container';
-	if (! empty($additional_class)) {
-		$classes .= ' ' . $additional_class;
-	}
-
-	ob_start();
-	include plugin_dir_path(__FILE__) . '../build/container/render.php';
-	return ob_get_clean();
-}
-
-/**
  * Render callback for the content-wrapper block.
  *
  * @param array  $attributes Block attributes.
@@ -58,32 +35,55 @@ function fsblocks_render_content_wrapper_block($attributes, $content)
 }
 
 /**
+ * Render callback for the container block.
+ *
+ * @param array  $attributes Block attributes.
+ * @param string $content    Inner block markup.
+ * @return string            HTML markup.
+ */
+// function fsblocks_render_container_block($attributes, $content)
+// {
+// 	$additional_class = isset($attributes['additionalClass'])
+// 		? sanitize_html_class($attributes['additionalClass'])
+// 		: '';
+
+// 	$classes = 'container';
+// 	if (! empty($additional_class)) {
+// 		$classes .= ' ' . $additional_class;
+// 	}
+
+// 	ob_start();
+// 	include plugin_dir_path(__FILE__) . '../build/container/render.php';
+// 	return ob_get_clean();
+// }
+
+/**
  * Render callback for the row block.
  */
-function fsblocks_render_row_block($attributes, $content)
-{
-	$additional_classes = (isset($attributes['additionalClasses']) && is_array($attributes['additionalClasses']))
-		? $attributes['additionalClasses']
-		: [];
+// function fsblocks_render_row_block($attributes, $content)
+// {
+// 	$additional_classes = (isset($attributes['additionalClasses']) && is_array($attributes['additionalClasses']))
+// 		? $attributes['additionalClasses']
+// 		: [];
 
-	ob_start();
-	include plugin_dir_path(__FILE__) . '../build/row/render.php';
-	return ob_get_clean();
-}
+// 	ob_start();
+// 	include plugin_dir_path(__FILE__) . '../build/row/render.php';
+// 	return ob_get_clean();
+// }
 
 /**
  * Render callback for the column block.
  */
-function fsblocks_render_column_block($attributes, $content)
-{
-	$additional_classes = (isset($attributes['additionalClasses']) && is_array($attributes['additionalClasses']))
-		? $attributes['additionalClasses']
-		: [];
+// function fsblocks_render_column_block($attributes, $content)
+// {
+// 	$additional_classes = (isset($attributes['additionalClasses']) && is_array($attributes['additionalClasses']))
+// 		? $attributes['additionalClasses']
+// 		: [];
 
-	ob_start();
-	include plugin_dir_path(__FILE__) . '../build/column/render.php';
-	return ob_get_clean();
-}
+// 	ob_start();
+// 	include plugin_dir_path(__FILE__) . '../build/column/render.php';
+// 	return ob_get_clean();
+// }
 
 /**
  * Render callback for the Accordion block.
