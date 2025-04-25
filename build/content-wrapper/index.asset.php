964818af574d4e825af7f6f734b3b02f9aaa8b65
Rory <rory@fancysquares.com>
2025-04-24 19:01:53 -0700

    Update vanilla build files

--- build/content-wrapper/index.asset.php
+++ build/content-wrapper/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a25b43458bd9c46ee8f5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2ee6e7ede8dedb58e955');
